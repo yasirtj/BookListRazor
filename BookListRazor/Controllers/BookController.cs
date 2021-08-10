7681b226943b4ce1ad2b6844a05e76797d68f8bb
Yasir Mohammednour <yasirmohammednour@hotmail.com>
2021-08-10 20:04:54 +0200

    Add project files.

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var BookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (BookFromDb == null )
+            {
+                return Json(new { success = false, message = "Error While Deleting" });
+            }
+            _db.Book.Remove(BookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful"});
+        }
+    }
+}
